dbbdb7d1c34b520db72570465fcecabf95045257
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2021-06-08 11:57:46 +0300

    feature: Add Article content type.

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/components'
+    '~/plugins/components',
+    '~/plugins/composition-api.js',
+    '~/plugins/storyblok-rich-text-renderer.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
