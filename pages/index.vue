be77e22e3e79d594a7f81183161964799313cc46
Tsegaselassie Tadesse <tsegaselassi@gmail.com>
2021-06-08 12:12:03 +0300

    feature: Add Featured Articles component.

--- pages/index.vue
+++ pages/index.vue
@@ -58,6 +58,15 @@ export default {
       }
     });
   },
+  async fetch(context) {
+    // Loading reference data - Articles in our case
+    if(context.store.state.articles.loaded !== '1') {
+
+      let articlesRefRes = await context.app.$storyapi.get(`cdn/stories/`, { starts_with: 'articles/', version: 'draft' })
+      context.store.commit('articles/setArticles', articlesRefRes.data.stories)
+      context.store.commit('articles/setLoaded', '1')
+    }
+  },
 }
 </script>
 
